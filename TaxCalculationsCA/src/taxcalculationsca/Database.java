b111bfd7502bb6276cc0c0d9cba3ce6556346a74
2022236 <146198723+2022236@users.noreply.github.com>
2023-12-13 14:00:42 +0000

    Update Database.java
    A few details that were initially wrong

--- TaxCalculationsCA/src/taxcalculationsca/Database.java
+++ TaxCalculationsCA/src/taxcalculationsca/Database.java
@@ -10,10 +10,10 @@ package taxcalculationsca;
  */
 public abstract class Database {
    protected final static String dbBaseURL = "jdbc:mysql://localhost";
-   protected final static String USER = "OOC2023";
+   protected final static String USER = "ooc2023";
    protected final static String PASSWORD = "ooc2023";
-   protected final static String dbName = "TaxCalculation";
-   protected final static String tableName = "userDetails";
+   protected final static String dbName = "mydb";
+   protected final static String tableName = "user";
    protected final static String db_URL = dbBaseURL + "/" + dbName;
    
    
